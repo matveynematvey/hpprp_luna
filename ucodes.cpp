eda9e93ca242e0a318729e0b2940de6398d6d444
Anton <kondratov@mail.ru>
2021-06-10 13:05:31 +0700

    Точно всё работает, но много мусора

--- ucodes.cpp
+++ ucodes.cpp
@@ -12,11 +12,11 @@
 #define RP2 32
 #define RP3 64
 #define RP4 128
-#define WIDTH 100
-#define HEIGHT 100
+#define WIDTH 500
+#define HEIGHT 500
 #define ITERS 4
-#define AVERAGING_RADIUS 1
-#define SOURCE_WIDTH 20
+#define AVERAGING_RADIUS 4
+#define SOURCE_WIDTH 10
 #define FIELD(i, j) field[(i)*(WIDTH) + j] //i-строка j-столбец
 #define NEW_FIELD(i, j) new_field[(i)*(WIDTH) + j]
 
@@ -43,7 +43,7 @@ void fill(char *field, int i0, int j0, int i1, int j1,
 	i_e -= i00; //end of the strip in local area
 	
 	int i, j;
-	for (i = i_s; i < i_e; i++)
+	for (i = i_s + AVERAGING_RADIUS; i < i_e + AVERAGING_RADIUS; i++)
 	{
 		for (j = j_s; j < j_e; j++)
 		{
@@ -72,7 +72,7 @@ void fill(char *field, int i0, int j0, int i1, int j1,
 				}
 			}*/
 			
-			FIELD(i, j)=cell;
+			FIELD(i + AVERAGING_RADIUS, j)=cell;
 		}
 	}
 }
@@ -90,7 +90,7 @@ void fillS(char *field, int i0, int j0, int i1, int j1,
 	i_e -= i00; //end of the strip in local area
 	
 	int i, j;
-	for (i = i_s; i < i_e; i++)
+	for (i = i_s + AVERAGING_RADIUS; i < i_e + AVERAGING_RADIUS; i++)
 	{
 		for (j = j_s; j < j_e; j++)
 		{
@@ -115,7 +115,7 @@ void fillS(char *field, int i0, int j0, int i1, int j1,
 			//printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", FIELD(i + 1 - (HEIGHT / splits) * (split - 1), j), split, iter, i, j, ens, 0);
 			//fflush(stdout);
 			
-			FIELD(i, j)=cell;
+			FIELD(i + AVERAGING_RADIUS, j)=cell;
 			
 			//printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", FIELD(i + 1 - (HEIGHT / splits) * (split - 1), j), split, iter, i, j, ens, 1);
 			//fflush(stdout);
@@ -210,6 +210,11 @@ extern "C"
 
 	void Calculation(int ensemble, int split, int iter, InputDF &fi1, InputDF &fi2, OutputDF &dfo)
 	{
+
+
+		printf("CALC_%d\n", ensemble);
+		fflush(stdout);
+
 		//printf("%d\t%d\t%d\n", ensemble, split, iter);
 		//fflush(stdout);
 		double *DenMoveRest = dfo.create<double>(WIDTH*6, 0);
@@ -251,9 +256,9 @@ extern "C"
 				fl_rest=fopen(rest_path, "w");
 		}
 		
-		for (j=AVERAGING_RADIUS; j<WIDTH-AVERAGING_RADIUS; j++) {
+		for (j=AVERAGING_RADIUS; j<WIDTH-AVERAGING_RADIUS - 1; j++) {
 			int rest=0, move=0;
-			for (i=rad1; i < (HEIGHT / splits) - rad2; i++ ) {
+			for (i=rad1; i < (HEIGHT / splits) - rad2 - 1; i++ ) {
 				SumMass(i, j, &rest, &move, field);
 			}
 
@@ -261,25 +266,25 @@ extern "C"
 				double new_density=1.0*(rest+move)/(HEIGHT-AVERAGING_RADIUS*2-1)/square;
 				double new_move=1.0*move/(HEIGHT-AVERAGING_RADIUS*2-1)/square;
 				double new_rest=1.0*rest/(HEIGHT-AVERAGING_RADIUS*2-1)/square;
-				DenMoveRest[j + index * 3] = (new_density + OldDenMoveRest[j] +(ensemble - 1)*OldDenMoveRest[j + index * 3])/(ensemble);
-				DenMoveRest[j + index * 4] = (new_move + OldDenMoveRest[j + index] +(ensemble - 1)*OldDenMoveRest[j + index * 4])/(ensemble);
-				DenMoveRest[j + index * 5] = (new_rest + OldDenMoveRest[j + index * 2] + (ensemble - 1)*OldDenMoveRest[j + index * 5])/(ensemble);
+				DenMoveRest[j - AVERAGING_RADIUS + index * 3] = (new_density + OldDenMoveRest[j - AVERAGING_RADIUS] +(ensemble - 1)*OldDenMoveRest[j - AVERAGING_RADIUS + index * 3])/(ensemble);
+				DenMoveRest[j - AVERAGING_RADIUS + index * 4] = (new_move + OldDenMoveRest[j - AVERAGING_RADIUS + index] +(ensemble - 1)*OldDenMoveRest[j - AVERAGING_RADIUS + index * 4])/(ensemble);
+				DenMoveRest[j - AVERAGING_RADIUS + index * 5] = (new_rest + OldDenMoveRest[j - AVERAGING_RADIUS + index * 2] + (ensemble - 1)*OldDenMoveRest[j - AVERAGING_RADIUS + index * 5])/(ensemble);
 			}
 			else{
-				DenMoveRest[j] = 1.0*(rest+move)/(HEIGHT-AVERAGING_RADIUS*2-1)/square + OldDenMoveRest[j];
-				DenMoveRest[j + index] = 1.0*move/(HEIGHT-AVERAGING_RADIUS*2-1)/square + OldDenMoveRest[j + index];
-				DenMoveRest[j + index * 2] = 1.0*rest/(HEIGHT-AVERAGING_RADIUS*2-1)/square + OldDenMoveRest[j + 2*index];
-				DenMoveRest[j + index * 3] = OldDenMoveRest[j + index * 3];
-				DenMoveRest[j + index * 4] = OldDenMoveRest[j + index * 4];
-				DenMoveRest[j + index * 5] = OldDenMoveRest[j + index * 5];
+				DenMoveRest[j - AVERAGING_RADIUS] = 1.0*(rest+move)/(HEIGHT-AVERAGING_RADIUS*2-1)/square + OldDenMoveRest[j- AVERAGING_RADIUS];
+				DenMoveRest[j - AVERAGING_RADIUS + index] = 1.0*move/(HEIGHT-AVERAGING_RADIUS*2-1)/square + OldDenMoveRest[j - AVERAGING_RADIUS+ index];
+				DenMoveRest[j - AVERAGING_RADIUS + index * 2] = 1.0*rest/(HEIGHT-AVERAGING_RADIUS*2-1)/square + OldDenMoveRest[j - AVERAGING_RADIUS + 2*index];
+				DenMoveRest[j - AVERAGING_RADIUS + index * 3] = OldDenMoveRest[j - AVERAGING_RADIUS+ index * 3];
+				DenMoveRest[j - AVERAGING_RADIUS + index * 4] = OldDenMoveRest[j- AVERAGING_RADIUS + index * 4];
+				DenMoveRest[j - AVERAGING_RADIUS + index * 5] = OldDenMoveRest[j - AVERAGING_RADIUS + index * 5];
 			}
 			
 			//printf("%d\t%d\t%d\t%d\n", split, iter, i, DenMoveRest[j]);
 			//fflush(stdout);
 			if(ensemble == max_ensemble){
-				fprintf(fl_density, "%d\t%lf\n", j , DenMoveRest[j + index * 3]);
-				fprintf(fl_move, "%d\t%lf\n", split, DenMoveRest[j + index * 4]);
-				fprintf(fl_rest, "%d\t%lf\n", split, DenMoveRest[j + index * 5]);
+				fprintf(fl_density, "%d\t%lf\n", j , DenMoveRest[j - AVERAGING_RADIUS + index * 3]);
+				fprintf(fl_move, "%d\t%lf\n", split, DenMoveRest[j - AVERAGING_RADIUS + index * 4]);
+				fprintf(fl_rest, "%d\t%lf\n", split, DenMoveRest[j - AVERAGING_RADIUS + index * 5]);
 			}
 		}
 		if(ensemble == max_ensemble){
@@ -296,23 +301,25 @@ extern "C"
 		fclose(fl_old);*/
 	}
 
-	void c_iprint(int n, int k, InputDF &fi)
+	void c_iprint(int a, int n, int j, int k )//InputDF &fi
 	{
 		//int data = fi.getValue<int>();
-		char* data = fi.getData<char>();	
-		for (int i=0; i < n; i++)
+		//char* data = fi.getData<char>();	
+		/*for (int i=0; i < n; i++)
 		{
 			printf("%d\t%d\t%d\n",i, k, data[i]);
 			fflush(stdout);
 		}
 		printf("\n\n");
-			fflush(stdout);
+			fflush(stdout);*/
+		printf("%d\t%d\t%d\t%d\n",a, n, j, k);
+		fflush(stdout);
 	}
 
 	void SetField(int split, int ens, OutputDF &df)
 	{
-		char *field = df.create<char>(((HEIGHT / splits) + 2) * WIDTH, 0); //полоса
-		fill(field, (HEIGHT / splits) * (split - 1), 0, (HEIGHT / splits) * split, WIDTH, 0.7, 0.7, 0.7, 0.7, 0.25, 0, 0, 0, split);
+		char *field = df.create<char>(((HEIGHT / splits) + 2*AVERAGING_RADIUS) * WIDTH, 0); //полоса
+		fill(field, 0, 0, HEIGHT, WIDTH, 0.7, 0.7, 0.7, 0.7, 0.25, 0, 0, 0, split);
 
 		/*char field_path[100];
 		FILE *fl_field;
@@ -357,14 +364,17 @@ extern "C"
 
 	void SourceField(int split, int iter, int ens, InputDF &fi, OutputDF &df)
 	{
+		//printf("Source_S_%d_%d_%d\n", split, iter, ens);
+		//fflush(stdout);
 		char *field = fi.getData<char>();
 		df = fi;
 		if (iter == 1){
-			fillS(field, (HEIGHT / splits) * (split - 1), WIDTH/2-SOURCE_WIDTH/2, (HEIGHT / splits) * split, WIDTH/2+SOURCE_WIDTH/2, 1, 1, 1, 1, 0.75, 0, 0, 0, split, iter, ens);
+			fillS(field, 0, WIDTH/2-SOURCE_WIDTH/2, HEIGHT, WIDTH/2+SOURCE_WIDTH/2, 1, 1, 1, 1, 0.75, 0, 0, 0, split, iter, ens);
 			//printf("%d\t%d\t%d\t%d\t%d\t%d\n", split, iter, (HEIGHT / splits) * (split - 1), WIDTH/2-SOURCE_WIDTH/2, (HEIGHT / splits) * split, WIDTH/2+SOURCE_WIDTH/2);
 			//fflush(stdout);
 		}
-
+		//printf("Source_E_%d_%d_%d\n", split, iter, ens);
+		//fflush(stdout);
 		/*
 		char field_path[100];
 		FILE *fl_field;
@@ -395,22 +405,25 @@ extern "C"
 
 	void CollideCells(int split, int iter, int ens, InputDF &dfi, OutputDF &dfo, OutputDF &dfo1, OutputDF &dfo2)
 	{
-		
-		char *gr1 = dfo1.create<char>(WIDTH, 0);
-		char *gr2 = dfo2.create<char>(WIDTH, 0);
+		//printf("\tCOLLIDE_S_%d_%d_%d\n", split, iter, ens);
+		//fflush(stdout);
+		char *gr1 = dfo1.create<char>(WIDTH*AVERAGING_RADIUS, 0);
+		char *gr2 = dfo2.create<char>(WIDTH*AVERAGING_RADIUS, 0);
 		char *field = dfi.getData<char>();
 		dfo=dfi;
 		char *new_field = dfi.getData<char>();
 		
 		//for(int i=0; i<50; i++)
 		//	printf("%d\t%d\n", i, new_field[i]);
-		for (int i = WIDTH; i < ((HEIGHT / splits) + 1) * WIDTH; i++)
+		for (int i = WIDTH*AVERAGING_RADIUS; i < ((HEIGHT / splits) + AVERAGING_RADIUS) * WIDTH; i++)
 			new_field[i] = collideL(field[i]);
-		for (int i = WIDTH; i < WIDTH*2; i++)
-			gr1[i - WIDTH] = new_field[i];
-		for (int j = ((HEIGHT / splits)) * WIDTH, i = 0; j < ((HEIGHT / splits) + 1) * WIDTH; j++, i++)
+		for (int i = WIDTH*AVERAGING_RADIUS; i < WIDTH*(2*AVERAGING_RADIUS); i++)
+			gr1[i - WIDTH*AVERAGING_RADIUS] = new_field[i];
+		for (int j = ((HEIGHT / splits)) * WIDTH, i = 0; j < ((HEIGHT / splits) + AVERAGING_RADIUS) * WIDTH; j++, i++)
 			gr2[i] = new_field[j];
 
+	//printf("\tCOLLIDE_E_%d_%d_%d\n", split, iter, ens);
+	//	fflush(stdout);
 	/*
 	if(iter > 1){
 		char field_path[100];
@@ -457,7 +470,8 @@ extern "C"
 
 	void FillEdges(int split, int iter, int ens, InputDF &dfi, InputDF &dfi1, InputDF &dfi2, OutputDF &dfo)
 	{
-		
+		printf("FILL_S_%d_%d_%d\n", split, iter, ens);
+		fflush(stdout);
 		char *field = dfi.getData<char>();
 		char *gr1 = dfi1.getData<char>();
 		char *gr2 = dfi2.getData<char>();
@@ -465,11 +479,12 @@ extern "C"
 		char *new_field = dfi.getData<char>();
 		//for(int i=0; i<50; i++)
 		//	printf("%d\t%d\n", i, new_field[i]);
-		for (int i = 0; i < WIDTH; i++)
+		for (int i = 0; i < WIDTH*AVERAGING_RADIUS; i++)
 				new_field[i] = gr1[i];
-		for (int j = ((HEIGHT / splits) + 1) * WIDTH, i = 0; j < ((HEIGHT / splits) + 2) * WIDTH; j++, i++)
+		for (int j = ((HEIGHT / splits) + AVERAGING_RADIUS) * WIDTH, i = 0; j < ((HEIGHT / splits) + 2*AVERAGING_RADIUS) * WIDTH; j++, i++)
 				new_field[j] = gr2[i];
-
+		printf("FILL_E_%d_%d_%d\n", split, iter, ens);
+		fflush(stdout);
 		/*
 		char field_path[100];
 		FILE *fl_field;
@@ -484,8 +499,10 @@ extern "C"
 
 	void AssemblyNewLayer(int split, int iter, int ens, InputDF &dfi, OutputDF &dfo)
 {
+		printf("ANL_f_%d_%d_%d\n", split, iter, ens);
+		fflush(stdout);
 		char *field = dfi.getData<char>();
-		char *new_field = dfo.create<char>(((HEIGHT / splits) + 2) * WIDTH, 0);	
+		char *new_field = dfo.create<char>(((HEIGHT / splits) + 2*AVERAGING_RADIUS) * WIDTH, 0);	
 		//dfo = dfi;
 		//char *new_field = dfi.getData<char>();
 		
@@ -498,27 +515,29 @@ extern "C"
 		//	printf("%d\t%d\t%d\n", i, new_field[i], field[i]);
 		char field_path1[100];
 		FILE *fl_field1;
-		sprintf(field_path1, "Testing/NewField/%d_%d_%d_n.txt", ens, split, iter);
-		fl_field1=fopen(field_path1, "w");
-		for (int i=1; i < HEIGHT_N+1; i++) 
+		//sprintf(field_path1, "Testing/NewField/%d_%d_%d_n.txt", ens, split, iter);
+		//fl_field1=fopen(field_path1, "w");
+		for (int i=AVERAGING_RADIUS; i < HEIGHT_N+AVERAGING_RADIUS; i++) 
 		{
 			int j;
 			for (j=0; j<WIDTH; j++) 
 			{
 				char cell=FIELD(i, j)&0xf0; // keep rest mass
-				fprintf(fl_field1, "%d\t%d\t", FIELD(i, j), cell);
-				fflush(stdout);
+				//fprintf(fl_field1, "%d\t%d\t", FIELD(i, j), cell);
+				//fflush(stdout);
 				// propogate particles
 				if (FIELD(i, (j+WIDTH-1)%WIDTH)&RIGHT) cell+=RIGHT; //field[(i)*(WIDTH) + j]
 				if (FIELD(i, (j+1)%WIDTH)&LEFT) cell+=LEFT;
 				if (FIELD((i+HEIGHT_N-1)%HEIGHT_N, j)&UP) cell+=UP;
 				if (FIELD((i+1)%HEIGHT_N, j)&DOWN) cell+=DOWN;
 				NEW_FIELD(i, j)=cell;
-				fprintf(fl_field1, "%d\n", cell);
-				fflush(stdout);
+				//fprintf(fl_field1, "%d\n", cell);
+				//fflush(stdout);
 			}
 		}
-		fclose(fl_field1);
+		//fclose(fl_field1);
+		printf("ANL_s_%d_%d_%d\n", split, iter, ens);
+		fflush(stdout);
 		/*if(iter == 1 && ens == 1 && split == 1)
 		for(int i=0; i<100; i++){
 			printf("%d\t%d\n", i, new_field[i]);
